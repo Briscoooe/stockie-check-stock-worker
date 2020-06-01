cea3de9fbd344c5f4c0dedf03ba5cfda25774bf2
Brian Briscoe <brian@artech.holdings>
2020-06-01 22:13:12 +0100

    queries

--- queries.py
+++ queries.py
@@ -1,20 +1,19 @@
 get_products_to_scrape_query = """
-    SELECT product_id, name, url, variant_id, store_id, in_stock
+    SELECT id, name, url, variant_id, store_id, in_stock
     FROM products
     WHERE active = 1
-    AND deleted_at IS NULL
 """
 
 update_product_in_stock = """
     UPDATE products
     SET in_stock = %s,
     updated_at = NOW()
-    WHERE product_id = %s
+    WHERE id = %s
 """
 
 update_multiple_products_in_stock = """
     UPDATE products
     SET in_stock = %s,
     updated_at = NOW()
-    WHERE product_id IN (%s)
+    WHERE id IN (%s)
 """
